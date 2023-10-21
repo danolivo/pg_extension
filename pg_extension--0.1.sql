73ae68a2c441aeeddc6b19eb8e3ed9285a4755a4
Andrey V. Lepikhov <a.lepikhov@postgrespro.ru>
2023-10-21 22:56:09 +0700

    Add function which generates statistics by index;

--- pg_extension--0.1.sql
+++ pg_extension--0.1.sql
@@ -1,1 +1,6 @@
-CREATE TABLE test (x bigint, payload char(256));
+
+-- Pass through all the relations in the database and
+CREATE FUNCTION pg_catalog.build_extended_statistic(idxname text)
+RETURNS integer
+AS 'MODULE_PATHNAME', 'build_extended_statistic'
+LANGUAGE C VOLATILE STRICT;
--- pg_extension.c
+++ pg_extension.c
@@ -1,43 +1,143 @@
 #include "postgres.h"
 
-#include "executor/executor.h"
-#include "optimizer/paths.h"
+#include "access/relation.h"
+#include "catalog/namespace.h"
+#include "commands/defrem.h"
+//#include "nodes/execnodes.h" // To delete
+#include "utils/rel.h"
+#include "utils/varlena.h"
 
 PG_MODULE_MAGIC;
 
-static set_join_pathlist_hook_type prev_set_join_pathlist_hook = NULL;
-
+PG_FUNCTION_INFO_V1(build_extended_statistic);
 
 void _PG_init(void);
-static void try_joinswitcher(PlannerInfo *root, RelOptInfo *joinrel,
-							 RelOptInfo *outerrel, RelOptInfo *innerrel,
-							 JoinType jointype, JoinPathExtraData *extra);
+#include "catalog/dependency.h"
+#include "catalog/index.h"
+#include "catalog/pg_extension.h"
+#include "commands/extension.h"
+#include "nodes/makefuncs.h"
+#include "utils/lsyscache.h"
 
+/*
+ * generateClonedExtStatsStmt
+ */
+Datum
+build_extended_statistic(PG_FUNCTION_ARGS)
+{
+	text	   *relname = PG_GETARG_TEXT_PP(0);
+	RangeVar   *relvar;
+	Relation	rel;
+	TupleDesc	tupdesc;
+	Oid			indexId;
+	Oid			heapId;
+	IndexInfo  *indexInfo;
+	ObjectAddress obj;
+
+	/* Get descriptor of incoming index relation */
+	relvar = makeRangeVarFromNameList(textToQualifiedNameList(relname));
+	rel = relation_openrv(relvar, AccessShareLock);
+
+	if (rel->rd_rel->relkind != RELKIND_INDEX &&
+		rel->rd_rel->relkind != RELKIND_PARTITIONED_INDEX)
+		ereport(ERROR,
+				(errcode(ERRCODE_WRONG_OBJECT_TYPE),
+				 errmsg("\"%s\" is not an index",
+						RelationGetRelationName(rel))));
+
+	indexId = RelationGetRelid(rel);
+	tupdesc = CreateTupleDescCopy(RelationGetDescr(rel));
+	indexInfo = BuildIndexInfo(rel);
+	relation_close(rel, AccessShareLock);
+
+	if (indexInfo->ii_NumIndexKeyAttrs < 2)
+	{
+		FreeTupleDesc(tupdesc);
+		pfree(indexInfo);
+		PG_RETURN_INT32(0);
+	}
+
+	/*
+	 * At first, we need to identify what it isn't overlap another statistics.
+	 */
+
+	/*
+	 * Here is we form a statement to build statistics.
+	 */
+	{
+		CreateStatsStmt	   *stmt = makeNode(CreateStatsStmt);
+		ListCell		   *indexpr_item = list_head(indexInfo->ii_Expressions);
+		RangeVar		   *from;
+		int					i;
+
+		heapId = IndexGetRelation(indexId, false);
+		rel = relation_open(heapId, AccessShareLock);
+		from = makeRangeVar(get_namespace_name(RelationGetNamespace(rel)),
+								pstrdup(RelationGetRelationName(rel)), -1),
+		relation_close(rel, AccessShareLock);
+
+		stmt->defnames = NULL;		/* qualified name (list of String) */
+		stmt->exprs = NIL;
+		stmt->stat_types = list_make3(makeString("ndistinct"),
+									  makeString("dependencies"),
+									  makeString("mcv"));
+
+		for (i = 0; i < indexInfo->ii_NumIndexKeyAttrs; i++)
+		{
+			AttrNumber	atnum = indexInfo->ii_IndexAttrNumbers[i];
+			StatsElem  *selem = makeNode(StatsElem);
+
+			if (atnum != 0)
+			{
+				selem->name = pstrdup(tupdesc->attrs[i].attname.data);
+				selem->expr = NULL;
+			}
+			else
+			{
+				Node	   *indexkey;
+
+				indexkey = (Node *) lfirst(indexpr_item);
+				Assert(indexkey != NULL);
+				indexpr_item = lnext(indexInfo->ii_Expressions, indexpr_item);
+				selem->name = NULL;
+				selem->expr = indexkey;
+			}
+
+			stmt->exprs = lappend(stmt->exprs, selem);
+		}
+
+		/* Still only one relation allowed in the core */
+		stmt->relations = list_make1(from);
+		stmt->stxcomment = NULL;
+		stmt->transformed = false;	/* true when transformStatsStmt is finished */
+		stmt->if_not_exists = false;
+
+		obj = CreateStatistics(stmt);
+		if (!OidIsValid(obj.classId))
+		{
+			FreeTupleDesc(tupdesc);
+			pfree(indexInfo);
+			PG_RETURN_INT32(0);
+		}
+		else
+		{
+			ObjectAddress refobj;
+
+			refobj.classId = ExtensionRelationId;
+			refobj.objectId = get_extension_oid("pg_extension", false);
+			refobj.objectSubId = 0;
+			recordDependencyOn(&obj, &refobj, DEPENDENCY_AUTO);
+		}
+		list_free_deep(stmt->relations);
+	}
+
+	FreeTupleDesc(tupdesc);
+	pfree(indexInfo);
+	PG_RETURN_INT32(1);
+}
 
 void
 _PG_init(void)
 {
-	prev_set_join_pathlist_hook = set_join_pathlist_hook;
-	set_join_pathlist_hook = try_joinswitcher;
-
-	elog(LOG, "Template extension was initialized.");
-}
-
-static void
-try_joinswitcher(PlannerInfo *root,
-				 RelOptInfo *joinrel,
-				 RelOptInfo *outerrel,
-				 RelOptInfo *innerrel,
-				 JoinType jointype,
-				 JoinPathExtraData *extra)
-{
-	/*
-	 * Some extension intercept this hook earlier. Allow it to do a work
-	 * before us.
-	 */
-	if (prev_set_join_pathlist_hook)
-		(*prev_set_join_pathlist_hook)(root, joinrel, outerrel, innerrel,
-									   jointype, extra);
-
-	elog(DEBUG5, "Try to do something in the hook");
+	return;
 }
